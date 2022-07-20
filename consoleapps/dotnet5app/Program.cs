ae2e263634b00f3922358a911a50ff80e6b571de
Jesse Lawrence <jesse152@revature.net>
2022-07-20 15:32:54 -0500

    incoming

---
+++ consoleapps/dotnet5app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet5app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holding hands while running through the valley of shadow and death!");
+        }
+    }
+}
